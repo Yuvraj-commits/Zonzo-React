29d10d6bedbe8a0c6ecc9a47637a0c2e3e094961
Yuvraj-commits <yuvraj.rajput@applore.in>
2022-06-07 17:17:12 +0530

    Dashboard top  card added

--- src/components/DashBoard/Graph/DashboardGraph.js
+++ src/components/DashBoard/Graph/DashboardGraph.js
@@ -1,6 +1,8 @@
 import React from "react";
+import ArrowDropUpIcon from '@material-ui/core/';
 import styles from "./DashboardGraph.module.css";
 import { connect } from "react-redux";
+import "chart.js/auto";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,7 +12,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { Bar, Chart } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -22,81 +24,92 @@ ChartJS.register(
 );
 
 const DashboardGraph = () => {
-  const state = {
+  const data = {
     labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     datasets: [
       {
-        label: "Statistics This week",
-        barPercentage: 0.1,
-        barThickness: 40,
-        maxBarThickness: 50,
-        minBarLength: 5,
-        lineTension: 0.5,
-        backgroundColor: "#DDDDDD",
-        fill: true,
-        borderRadius: 12,
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 0,
-        data: [65, 59, 80, 81, 56, 10, 20],
+        label: "",
+        data: [0, 5, 10, 6, 16, 13, 19],
+        cubicInterpolationMode: "monotone",
+        tension: 0.4,
+        backgroundColor: "#FFFFFF",
+        borderColor: "#370D6F",
       },
     ],
   };
-
-  var options = {
+  const options = {
     scales: {
-      xAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
+      x: {
+        grid: {
+          color: "#E0E0E0",
+          borderColor: "#E0E0E0",
+          borderDash: [1, 4],
         },
-      ],
-      yAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
+      },
+      y: {
+        grid: {
+          color: "#E0E0E0",
+          borderColor: "#E0E0E0",
+          borderDash: [1, 4],
         },
-      ],
+      },
     },
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.upperGraphCards}>
         <div className={styles.column}>
           <div className={styles.card}>
-            <h1>hi</h1>
+            <div className={styles.cardWidthSixty}>
+              <h2>Today's Check Ins</h2>
+              <h1>158</h1>
+              <div>12% This Week</div>
+            </div>
+            <div className={styles.cardWidthForty}>
+              <div className={styles.circle}></div>
+              <div className={styles.circletwo}></div>
+            </div>
           </div>
         </div>
         <div className={styles.column}>
           <div className={styles.card}>
-            <h1>hi</h1>
+          <div className={styles.cardWidthSixty}>
+              <h2>Today's Revenue</h2>
+              <h1>300k</h1>
+              <div>12% This Week</div>
+            </div>
+            <div className={styles.cardWidthForty}>
+              <div
+                className={`${styles.circle} ${styles.greenBackground}`}
+              ></div>
+              <div
+                className={`${styles.circletwo} ${styles.greenLightBg}`}
+              ></div>
+            </div>
           </div>
         </div>
+
         <div className={styles.column}>
           <div className={styles.card}>
-            <h1>hi</h1>
+          <div className={styles.cardWidthSixty}>
+              <h2>Today's Bookings</h2>
+              <h1>7k</h1>
+              <div>12% This Week</div>
+            </div>
+            <div className={styles.cardWidthForty}>
+              <div
+                className={`${styles.circle} ${styles.orangeBackground}`}
+              ></div>
+              <div
+                className={`${styles.circletwo} ${styles.orangeLightBg}`}
+              ></div>
+            </div>
           </div>
         </div>
       </div>
       <div className={styles.lowerGraph}>
-        <Bar
-          data={state}
-          width={150}
-          height={50}
-          options={{
-            title: {
-              display: true,
-              text: "Average Rainfall per month",
-              fontSize: 10,
-            },
-            legend: {
-              display: true,
-              position: "right",
-            },
-            
-          }}
-        />
+        <Line data={data} options={options} height={130}/>
       </div>
     </div>
   );
